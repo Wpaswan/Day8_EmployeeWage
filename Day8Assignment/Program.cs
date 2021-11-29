491a591113db00edd404ab862f1d627ed75aba91
Wpaswan <dablumcapaswan@gmail.com>
2021-11-29 09:42:33 -0800

    [Dablu modified Monthly Wage]

--- Day8Assignment/Program.cs
+++ Day8Assignment/Program.cs
@@ -1,30 +1,9 @@
-﻿
+﻿using Day8Assignment;
 Console.WriteLine("Welcome To Employee Wage Computation!");
 
-int isPresent = 1;
-int Is_Full_Time = 1;
-int emp_hrs = 0;
-int empWage=0;
-int Emp_Rate_per_Hour = 20;
-Random random = new Random();
-int randominput = random.Next(0, 2);
 
-switch (randominput)
-{
-    case 1:
-is_Full_Time:
-        emp_hrs = 8;
-        break;
 
-    case 2:
-is_Part_Time:
-        emp_hrs = 4;
-        break;
+        MonthlyWages obj=new MonthlyWages();
+        obj.input3();
 
-    default:
-        emp_hrs = 0;
-        break;
-}
-
-empWage=emp_hrs*Emp_Rate_per_Hour;
-Console.WriteLine("Employee Wage:"+empWage);
+    
