0bc59183f1ab111520e9e58b2c02b4fff6eb8fb9
Wpaswan <dablumcapaswan@gmail.com>
2021-11-29 08:33:42 -0800

    [Dablu modified calculate wage]

--- Day8Assignment/Program.cs
+++ Day8Assignment/Program.cs
@@ -2,15 +2,20 @@
 Console.WriteLine("Welcome To Employee Wage Computation!");
 
 int isPresent = 1;
+int Is_Full_Time = 1;
+int emp_hrs = 0;
+int empWage=0;
+int Emp_Rate_per_Hour = 20;
 Random random = new Random();
 int randominput = random.Next(0, 2);
 
 if (randominput == isPresent)
 {
-    Console.WriteLine("Present");
+    emp_hrs=8;
 }
 else
 {
-    Console.WriteLine("Absent");
+    emp_hrs = 0;
 }
-Console.ReadLine();
+empWage=emp_hrs*Emp_Rate_per_Hour;
+Console.WriteLine("Employee Wage:"+empWage);
