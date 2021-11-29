2bfc89d1678cd2e3b87ca3eff02f4a4b5bb17919
Wpaswan <dablumcapaswan@gmail.com>
2021-11-29 08:11:30 -0800

    [Dablu modified the code for attendance]

--- Day8Assignment/Program.cs
+++ Day8Assignment/Program.cs
@@ -1,2 +1,16 @@
 ﻿
 Console.WriteLine("Welcome To Employee Wage Computation!");
+
+int isPresent = 1;
+Random random = new Random();
+int randominput = random.Next(0, 2);
+
+if (randominput == isPresent)
+{
+    Console.WriteLine("Present");
+}
+else
+{
+    Console.WriteLine("Absent");
+}
+Console.ReadLine();
