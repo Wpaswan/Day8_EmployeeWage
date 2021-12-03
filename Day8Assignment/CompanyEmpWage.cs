d34d79aa305411fc0f9c74aaf0590f1a02476a91
Wpaswan <dablumcapaswan@gmail.com>
2021-12-03 09:25:09 -0800

    [Dablu modified the code using class Emp Wage]

---
+++ Day8Assignment/CompanyEmpWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Assignment
+{
+    internal class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHourPerMonth;
+        public int totalEmpWage;
+        public CompanyEmpWage(string company,int empRatePerHour, int maxHourPerMonth, int totalEmpWage)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.maxHourPerMonth = maxHourPerMonth;
+            this.totalEmpWage = totalEmpWage;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage=totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :"+this.company+" is:"+this.totalEmpWage;
+        }
+        
+    }
+}
